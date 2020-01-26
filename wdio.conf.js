4107eb1067b200899727c13a668e2a160c8c5c41
malarvizhi178 <31912391+malarvizhi178@users.noreply.github.com>
2020-01-26 15:12:29 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -9,7 +9,7 @@ exports.config = {
     //runner: 'local',
     //
     // Override default path ('/wd/hub') for chromedriver service.
-    host: "selenium-hub",
+    hostname: "selenium-hub",
     path: "/wd/hub",
     port: 4444,
     //
